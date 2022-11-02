9821c6ed8fb3c366d7d9ce582ada15317321e282
qwesdme <qwesdme@gmail.com>
2022-11-02 18:06:57 +0300

    pure string

--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module github.com/qwesdme/csgo-parser-server
 
 go 1.19
 
+// replace github.com/markus-wa/demoinfocs-golang/v3 v3.0.1 => C:\Users\fath\Documents\code\go\demoinfocs-golang
+
 require github.com/gofiber/fiber/v2 v2.39.0
 
 require (
 	github.com/gofiber/websocket/v2 v2.1.1
-	github.com/markus-wa/demoinfocs-golang/v2 v2.13.3
+	github.com/markus-wa/demoinfocs-golang/v3 v3.0.1
+	github.com/markus-wa/godispatch v1.4.1
 )
 
 require (
@@ -17,12 +20,12 @@ require (
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/markus-wa/go-unassert v0.1.2 // indirect
 	github.com/markus-wa/gobitread v0.2.3 // indirect
-	github.com/markus-wa/godispatch v1.4.1 // indirect
 	github.com/markus-wa/ice-cipher-go v0.0.0-20220126215401-a6adadccc817 // indirect
 	github.com/markus-wa/quickhull-go/v2 v2.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/oklog/ulid/v2 v2.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/savsgio/gotils v0.0.0-20211223103454-d0aaa54c5899 // indirect
@@ -30,4 +33,5 @@ require (
 	github.com/valyala/fasthttp v1.40.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	dispatch "github.com/markus-wa/godispatch"
 	"log"
 	"os"
+	strconv "strconv"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
-	dem "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs"
-	"github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/common"
-	"github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/events"
+	dem "github.com/markus-wa/demoinfocs-golang/v3/pkg/demoinfocs"
+	"github.com/markus-wa/demoinfocs-golang/v3/pkg/demoinfocs/common"
+	"github.com/markus-wa/demoinfocs-golang/v3/pkg/demoinfocs/events"
 )
 
 var parsers = map[string]dem.Parser{}
 var headers = map[string]common.DemoHeader{}
 var sockets = map[string]*websocket.Conn{}
 var mtInts = map[string]int{}
+
+var markedFrames = map[string][]int{}
+
+//var currentFrameToEnd int
 
 func main() {
 	app := fiber.New()
@@ -33,10 +39,6 @@ func main() {
 }
 
 func websocketHandler(c *websocket.Conn) {
-	log.Println(c.Locals("allowed"))  // true
-	log.Println(c.Query("v"))         // 1.0
-	log.Println(c.Cookies("session")) // ""
-
 	var (
 		mt  int
 		msg []byte
@@ -52,14 +54,12 @@ func websocketHandler(c *websocket.Conn) {
 		mtInts[path] = mt
 		sockets[path] = c
 		safe(performWebsocketTask(mapData))
-		//log.Printf("recv: %s", msg)
 	}
 }
 
 func performWebsocketTask(mapData map[string]string) error {
 	path := mapData["path"]
 	task := mapData["task"]
-	fmt.Println(task)
 	switch task {
 	case "test":
 		{
@@ -68,6 +68,10 @@ func performWebsocketTask(mapData map[string]string) error {
 	case "parse_to_end":
 		{
 			return parseToEnd(path)
+		}
+	case "parse_to_end_with_marked_frames":
+		{
+			return parseToEndWithMarkedFrames(path)
 		}
 	case "new_parser":
 		{
@@ -84,8 +88,11 @@ func performWebsocketTask(mapData map[string]string) error {
 	case "current_frame":
 		{
 			return currentFrame(path)
-
 		}
+	//case "current_playing":
+	//	{
+	//		return currentPlaying(path)
+	//	}
 	case "frame_rate":
 		{
 			return frameRate(path)
@@ -106,10 +113,22 @@ func performWebsocketTask(mapData map[string]string) error {
 		}
 	default:
 		{
-			prefix := "register_event_handler:"
-			if strings.HasPrefix(task, prefix) {
-				event := strings.TrimPrefix(task, prefix)
+			prefixRegister := "register_event_handler:"
+			if strings.HasPrefix(task, prefixRegister) {
+				event := strings.TrimPrefix(task, prefixRegister)
 				return registerEventHandler(path, event)
+			}
+
+			prefixUnregister := "unregister_event_handler:"
+			if strings.HasPrefix(task, prefixUnregister) {
+				handlerId := strings.TrimPrefix(task, prefixUnregister)
+				return unregisterEventHandler(path, handlerId)
+			}
+
+			prefixMarkFrames := "mark_frames:"
+			if strings.HasPrefix(task, prefixMarkFrames) {
+				frames := strings.TrimPrefix(task, prefixMarkFrames)
+				return markFrames(path, frames)
 			}
 			fmt.Println("Task unknown: " + task)
 			return sendError(path)
@@ -117,9 +136,22 @@ func performWebsocketTask(mapData map[string]string) error {
 	}
 }
 
+func markFrames(path string, framesStr string) error {
+	frameStrSplit := strings.Split(framesStr, ",")
+	var frames []int
+	for _, frameStr := range frameStrSplit {
+		frame, err := strconv.Atoi(frameStr)
+		safe(err)
+		frames = append(frames, frame)
+	}
+	markedFrames[path] = frames
+	return sendOk(path)
+}
+
 func sendMessage(path string, message string) error {
 	return sockets[path].WriteMessage(mtInts[path], []byte(message))
 }
+
 func sendOk(path string) error {
 	return sendMessage(path, "Ok")
 }
@@ -135,14 +167,47 @@ func closeParser(path string) error {
 	delete(mtInts, path)
 	delete(parsers, path)
 	delete(headers, path)
+	fmt.Printf("Done: %s\n", path)
 	return err
 }
 
 func currentFrame(path string) error {
 	parser := parsers[path]
 	tr := parser.CurrentFrame()
-	trs := fmt.Sprintf("%v\n", tr)
+	trs := fmt.Sprintf("%v", tr)
 	return sendMessage(path, trs)
+}
+
+//func currentPlaying(path string) error {
+//	playingStr := playingStr(path)
+//	return sendMessage(path, playingStr)
+//}
+
+func playingStr(path string) string {
+	parser := parsers[path]
+	playing := parser.GameState().Participants().Playing()
+
+	playingStr := ""
+	for i, player := range playing {
+		if i > 0 {
+			playingStr += "\n"
+		}
+		playingStr += fmt.Sprintf("%v,%v,%v,%s,", player.UserID, player.FlashDuration, player.SteamID64, player.Name)
+		playingStr += fmt.Sprintf("%v,%v,%v,", player.LastAlivePosition.X, player.LastAlivePosition.Y, player.LastAlivePosition.Z)
+		playerVelocity := player.Velocity()
+		playingStr += fmt.Sprintf("%v,%v,%v,", playerVelocity.X, playerVelocity.Y, playerVelocity.Z)
+		activeWeapon := player.ActiveWeapon()
+		var weaponName string
+		if activeWeapon == nil {
+			weaponName = "Unknown"
+		} else {
+			weaponName = activeWeapon.String()
+		}
+		playingStr += fmt.Sprintf("%s,", weaponName)
+		playingStr += fmt.Sprintf("%v,%v,", player.ViewDirectionX(), player.ViewDirectionY())
+		playingStr += fmt.Sprintf("%v,%v", player.IsDucking(), player.Health())
+	}
+	return playingStr
 }
 
 func parseNextFrame(path string) error {
@@ -158,29 +223,46 @@ func parseToEnd(path string) error {
 	safe(parsers[path].ParseToEnd())
 	return sendOk(path)
 }
+func parseToEndWithMarkedFrames(path string) error {
+	//currentFrameToEnd = 0
+	//safe(parsers[path].ParseToEnd())
+	var err error
+	p := parsers[path]
+	for ok := true; ok; ok, err = p.ParseNextFrame() {
+		safe(err)
+		currentFrame := p.CurrentFrame()
+		if isFrameExist(markedFrames[path], currentFrame) {
+			//markedFrame := fmt.Sprintf("event:MarkedFrame, frame:%v, %s", currentFrame, playingStr(path))
+			markedFrame := fmt.Sprintf("%v,%s", currentFrame, playingStr(path))
+			//fmt.Printf("event:MarkedFrame, frame:%v\n", currentFrame)
+			safe(sendMessage(path, markedFrame))
+		}
+	}
+	return sendOk(path)
+}
 
 func frameRate(path string) error {
 	header := headers[path]
 	fr := header.FrameRate()
-	frs := fmt.Sprintf("%v\n", fr)
+	frs := fmt.Sprintf("%v", fr)
 	return sendMessage(path, frs)
 }
 func inGameTick(path string) error {
 	parser := parsers[path]
 	tr := parser.GameState().IngameTick()
-	trs := fmt.Sprintf("%v\n", tr)
+	trs := fmt.Sprintf("%v", tr)
 	return sendMessage(path, trs)
 }
 
 func tickRate(path string) error {
 	parser := parsers[path]
 	tr := parser.TickRate()
-	trs := fmt.Sprintf("%v\n", tr)
+	trs := fmt.Sprintf("%v", tr)
 	return sendMessage(path, trs)
 }
 
 func newParser(path string) error {
-	fmt.Printf("%s\n", path)
+	fmt.Printf("New parser: %s\n", path)
 	f, err := os.Open(path)
 	safe(err)
 	parsers[path] = dem.NewParser(f)
@@ -190,7 +272,6 @@ func parseHeader(path string) error {
 	p := parsers[path]
 	h, err := p.ParseHeader()
 	headers[path] = h
-	fmt.Printf("%s\n", path)
 	safe(err)
 	return sendOk(path)
 }
@@ -198,59 +279,101 @@ func parseHeader(path string) error {
 func registerEventHandler(path string, event string) error {
 	switch event {
 	case "PlayerHurt":
-		parsers[path].RegisterEventHandler(func(e events.PlayerHurt) {
-			attacker := e.Attacker
-			var attackerId int
-			if attacker == nil {
-				attackerId = 0
-			} else {
-				attackerId = e.Attacker.UserID
-			}
+		{
+			parsers[path].RegisterEventHandler(func(e events.PlayerHurt) {
+				attacker := e.Attacker
+				var attackerId int
+				if attacker == nil {
+					attackerId = 0
+				} else {
+					attackerId = e.Attacker.UserID
+				}
 
-			player := e.Player
-			var playerId int
-			if player == nil {
-				playerId = 0
-			} else {
-				playerId = e.Player.UserID
-			}
+				player := e.Player
+				var playerId int
+				if player == nil {
+					playerId = 0
+				} else {
+					playerId = e.Player.UserID
+				}
 
-			weapon := e.Weapon
-			var weaponName string
-			if weapon == nil {
-				weaponName = "Unknown"
-			} else {
-				weaponName = e.Weapon.String()
-			}
+				weapon := e.Weapon
+				var weaponName string
+				if weapon == nil {
+					weaponName = "Unknown"
+				} else {
+					weaponName = e.Weapon.String()
+				}
 
-			playerHurt := fmt.Sprintf("event:PlayerHurt, attacker_id:%v, user_id:%v, weapon:%s", attackerId, playerId, weaponName)
-			safe(sendMessage(path, playerHurt))
-		})
+				frame := parsers[path].CurrentFrame()
+
+				playerHurt := fmt.Sprintf("event:PlayerHurt, attacker_id:%v, player_id:%v, weapon:%s, frame:%v", attackerId, playerId, weaponName, frame)
+				//fmt.Printf("%s\n", playerHurt)
+				safe(sendMessage(path, playerHurt))
+			})
+		}
 	case "WeaponFire":
-		parsers[path].RegisterEventHandler(func(e events.WeaponFire) {
-			weapon := e.Weapon
-			var weaponName string
-			if weapon == nil {
-				weaponName = "Unknown"
-			} else {
-				weaponName = e.Weapon.String()
-			}
+		{
+			parsers[path].RegisterEventHandler(func(e events.WeaponFire) {
+				weapon := e.Weapon
+				var weaponName string
+				if weapon == nil {
+					weaponName = "Unknown"
+				} else {
+					weaponName = e.Weapon.String()
+				}
 
-			shooter := e.Shooter
-			var shooterId int
-			if shooter == nil {
-				shooterId = 0
-			} else {
-				shooterId = e.Shooter.UserID
-			}
+				shooter := e.Shooter
+				var shooterId int
+				if shooter == nil {
+					shooterId = 0
+				} else {
+					shooterId = e.Shooter.UserID
+				}
 
-			weaponFire := fmt.Sprintf("event:WeaponFire, user_id:%v, weapon:%s", shooterId, weaponName)
-			safe(sendMessage(path, weaponFire))
-		})
+				frame := parsers[path].CurrentFrame()
+
+				weaponFire := fmt.Sprintf("event:WeaponFire, player_id:%v, weapon:%s, frame:%v", shooterId, weaponName, frame)
+				fmt.Printf("%s\n", weaponFire)
+				safe(sendMessage(path, weaponFire))
+			})
+		}
+	case "MarkedFrame":
+		{
+			//parsers[path].RegisterEventHandler(func(e any) {
+			//	currentFrame := parsers[path].CurrentFrame()
+			//	if currentFrame != currentFrameToEnd {
+			//		if isFrameExist(markedFrames[path], currentFrame) {
+			//			markedFrame := fmt.Sprintf("event:MarkedFrame, frame:%v, %s", currentFrame, playingStr(path))
+			//			fmt.Printf("event:MarkedFrame, frame:%v\n", currentFrame)
+			//			safe(sendMessage(path, markedFrame))
+			//		}
+			//	}
+			//})
+		}
 	default:
 		fmt.Println("Event unknown: " + event)
 		return sendError(path)
 	}
+	fmt.Printf("Registered:%s\n", event)
+	return sendOk(path)
+}
+
+func isFrameExist(s []int, i int) bool {
+	for _, v := range s {
+		if v == i {
+			return true
+		}
+	}
+	return false
+}
+
+func unregisterEventHandler(path string, handlerId string) error {
+	id, err := strconv.Atoi(handlerId)
+	safe(err)
+	var hid dispatch.HandlerIdentifier
+	hid = &id
+	parsers[path].UnregisterEventHandler(hid)
 	return sendOk(path)
 }
 
